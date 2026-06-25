bd2be8cfe6be3ce9f77cafa8d14a448c93d2a496
aeshaShawrma <ui24cs05@iiitsurat.ac.in>
2026-06-25 06:06:53 -0700

    Added llama requests in responsein the code itslef  so that you dont have to run it manually in terminal , instead gives output immediately when code runs . Also supported image format , earlier it was just supporting pdf format

--- main.py
+++ main.py
@@ -1,14 +1,53 @@
 import pdfplumber
+from pdf2image import convert_from_path
+import requests
+import os
+from pdf2image import convert_from_path
+from PIL import Image
 
-pdf_path= r"/* doc path */ "
-with pdfplumber.open(r"/* path */") as pdf:  #open and closing file 'with open(file) as f:'
-    text =""
+import pytesseract 
+pytesseract.pytesseract.tesseract_cmd = (
+     r"C:\Users\Aesha.sharma\AppData\Local\Programs\Tesseract-OCR\tesseract.exe"
+)
 
-    for page in pdf.pages:
-            page_text = page.extract_text()
-            if page_text:
-                text+=page_text +"\n"
-  
+
+
+file_path= r"C:\Users\Aesha.sharma\document_extraction\data\invoices\invoices\Screenshot 2026-06-25 052808.png"
+
+text =""
+
+extention = os.path.splitext(file_path)[1].lower()
+
+if extention in [".png" , "jpeg" , "jpg"]:
+    print("Image detected. Using OCR....")
+    image = Image.open(file_path)
+    text = pytesseract.image_to_string(image) 
+
+elif extention == ".pdf":
+    print("pdf detected ....")
+    try:
+        with pdfplumber.open(file_path) as pdf:  #open and closing file 'with open(file) as f:'
+
+            for page in pdf.pages:
+                page_text = page.extract_text()
+                if page_text:
+                    text+=page_text +"\n"
+    except Exception as e:
+        print("PDF extraction error :",e)
+
+    if len(text.strip()) < 50:
+        print("no text founf. USING OCR...")
+
+        pages = convert_from_path(file_path)
+        for page in pages:
+            page_text = pytesseract.image_to_string(page)
+            text += page_text +"\n"
+
+else :
+    print("unsupported file type")
+    exit()
+
+print("  \n                EXTRACTED TEXT                       \n ")
 print(text[:3000])
 
 prompt = f'''
@@ -21,3 +60,18 @@ prompt = f'''
     Document:
    '''
 
+#calling llama
+
+response = requests.post(
+    "http://localhost:11434/api/generate",
+    json={
+        "model" : "llama3",
+        "prompt" : prompt ,
+        "stream" : False
+    }
+)
+
+result = response.json()["response"]
+
+print("LLAMA Output")
+print(result)
